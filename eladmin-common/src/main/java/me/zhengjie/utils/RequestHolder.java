ece2c33057cc3af523beb9265ddbe021d9b7e5fe
杨彭伟 <hbwhypw@163.com>
2019-12-12 11:19:42 +0800

    升级Spring-boot版本

--- eladmin-common/src/main/java/me/zhengjie/utils/RequestHolder.java
+++ eladmin-common/src/main/java/me/zhengjie/utils/RequestHolder.java
@@ -3,6 +3,7 @@ package me.zhengjie.utils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 
 /**
@@ -14,5 +15,9 @@ public class RequestHolder {
 
     public static HttpServletRequest getHttpServletRequest() {
         return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+    }
+
+    public static HttpServletResponse getHttpServletResponse() {
+        return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getResponse();
     }
 }
